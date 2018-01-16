c61ef29b088b926ca937c9c67c73cb4123fcd2d3
Liz Tucksmith <tucks22e@mtholyoke.edu>
2018-01-16 10:18:36 -0500

    Initial commit

---
+++ app/src/main/java/Post.java
@@ -0,0 +1,15 @@
+import android.widget.Button;
+
+/**
+ * Created by demouser on 1/16/18.
+ */
+
+public class Post {
+    //Post, 285 character limit, view comments, upvote
+    //Function: type post/comment,
+    //Function: upvote/downvote
+
+    public Post(String text, int upvotes, int downvotes){
+
+    }
+}
